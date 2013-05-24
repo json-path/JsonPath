3cd4c3c2aab4c87388ee0e609282eace6fd5c7d8
Jochen Berger <fooberger@gmail.com>
2013-05-24 13:54:43 +0200

    convert rather than cast to List

--- json-path/src/main/java/com/jayway/jsonpath/internal/filter/ArrayQueryFilter.java
+++ json-path/src/main/java/com/jayway/jsonpath/internal/filter/ArrayQueryFilter.java
@@ -34,7 +34,7 @@ public class ArrayQueryFilter extends PathTokenFilter {
 
         Filter filter = filters.poll();
 
-        return filter.doFilter((List)obj, jsonProvider);
+        return filter.doFilter(jsonProvider.toList(obj), jsonProvider);
 
     }
 
