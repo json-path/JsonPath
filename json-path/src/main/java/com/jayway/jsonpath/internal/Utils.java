955042f92489bbb8bf31bf66cbd25a1d659ff934
Kalle Stenflo <kalle.stenflo@gmail.com>
2014-08-06 08:38:06 +0200

    Removed unused exceptions.

--- json-path/src/main/java/com/jayway/jsonpath/internal/Utils.java
+++ json-path/src/main/java/com/jayway/jsonpath/internal/Utils.java
@@ -247,62 +247,6 @@ public class Utils {
     // Converters
     //
     //---------------------------------------------------------
-
-    /**
-     * converts to Integer with radix 10
-     *
-     * @param o object to convert
-     * @return converted value
-     */
-    public static Integer toInt(Object o) {
-        if (null == o)
-            return null;
-        if (o instanceof Number)
-            return ((Number) o).intValue();
-        try {
-            return Integer.valueOf(o.toString().trim(), 10);
-        } catch (Exception e) {
-            throw new InvalidConversionException("Could not convert " + o.toString() + " to Integer");
-        }
-    }
-
-
-    /**
-     * converts to Long with radix 10
-     *
-     * @param o object to convert
-     * @return converted value
-     */
-    public static Long toLong(Object o) {
-        if (null == o)
-            return null;
-        if (o instanceof Number)
-            return ((Number) o).longValue();
-        try {
-            return Long.valueOf(o.toString().trim(), 10);
-        } catch (Exception e) {
-            throw new InvalidConversionException("Could not convert " + o.toString() + " to Long");
-        }
-    }
-
-    /**
-     * converts to Double with radix 10
-     *
-     * @param o object to convert
-     * @return converted value
-     */
-    public static Double toDouble(Object o) {
-        if (null == o)
-            return null;
-        if (o instanceof Number)
-            return ((Number) o).doubleValue();
-        try {
-            return Double.valueOf(o.toString().trim());
-        } catch (Exception e) {
-            throw new InvalidConversionException("Could not convert " + o.toString() + " to Double");
-        }
-    }
-
     public static String toString(Object o) {
         if (null == o) {
             return null;
