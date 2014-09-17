baa660478c895212a776492719743099ea5952e5
Kalle Stenflo <kalle.stenflo@gmail.com>
2014-09-17 19:55:21 +0200

    Fixed constructor in ConversionException

--- json-path/src/main/java/com/jayway/jsonpath/spi/converter/ConversionException.java
+++ json-path/src/main/java/com/jayway/jsonpath/spi/converter/ConversionException.java
@@ -2,17 +2,8 @@ package com.jayway.jsonpath.spi.converter;
 
 public class ConversionException extends RuntimeException {
 
-
-    public ConversionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
     public ConversionException(Throwable cause) {
         super(cause);
-    }
-
-    public ConversionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 
     public ConversionException(String message) {
