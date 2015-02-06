617e7754bed21705be17e4f2cf19852de5572051
Conor Gallagher <conor.gallagher@elasticpath.com>
2015-02-06 11:31:10 -0800

    Moving JsonProvider and MappingProvider implmentations out of the internal package and into the SPI. The external package is not exported in the OSGI manifest. This allows client code to configure different providers as intended.

--- json-path/src/main/java/com/jayway/jsonpath/spi/json/JacksonJsonProvider.java
+++ json-path/src/main/java/com/jayway/jsonpath/spi/json/JacksonJsonProvider.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jayway.jsonpath.internal.spi.json;
+package com.jayway.jsonpath.spi.json;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
--- json-path/src/main/java/com/jayway/jsonpath/spi/mapper/JacksonMappingProvider.java
+++ json-path/src/main/java/com/jayway/jsonpath/spi/mapper/JacksonMappingProvider.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jayway.jsonpath.internal.spi.mapper;
+package com.jayway.jsonpath.spi.mapper;
 
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
