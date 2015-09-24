616c5275b7b086541c19bf46bff2e09ffe8fa894
Archimedes Trajano <developer@trajano.net>
2015-09-24 00:50:35 -0400

    Remove unused logger from class
    Logger is not being used here.  Removing

--- json-path/src/main/java/com/jayway/jsonpath/Configuration.java
+++ json-path/src/main/java/com/jayway/jsonpath/Configuration.java
@@ -18,9 +18,6 @@ import com.jayway.jsonpath.internal.DefaultsImpl;
 import com.jayway.jsonpath.spi.json.JsonProvider;
 import com.jayway.jsonpath.spi.mapper.MappingProvider;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -35,8 +32,6 @@ import static java.util.Arrays.asList;
  * Immutable configuration object
  */
 public class Configuration {
-
-    private static final Logger logger = LoggerFactory.getLogger(Configuration.class);
 
     private static Defaults DEFAULTS = null;
 
