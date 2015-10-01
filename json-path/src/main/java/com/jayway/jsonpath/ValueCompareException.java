286aff4818810b32a7884fe2cdba8a558b770d0d
Archimedes Trajano <arch@trajano.net>
2015-10-01 18:31:04 -0400

    Alternate constructor for ValueCompareException
    This passes the message in the exception rather than being lost in the
    logs.

--- json-path/src/main/java/com/jayway/jsonpath/ValueCompareException.java
+++ json-path/src/main/java/com/jayway/jsonpath/ValueCompareException.java
@@ -19,6 +19,16 @@ public class ValueCompareException extends JsonPathException {
     public ValueCompareException() {
     }
 
+    /**
+     * Construct the exception with message capturing the classes for two objects.
+     *
+     * @param left first object
+     * @param right second object
+     */
+    public ValueCompareException(final Object left, final Object right) {
+        super(String.format("Can not compare a %1s with a %2s", left.getClass().getName(), right.getClass().getName()));
+    }
+
     public ValueCompareException(String message) {
         super(message);
     }
