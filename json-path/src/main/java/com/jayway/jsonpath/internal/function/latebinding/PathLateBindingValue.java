e8199bd93d05e8fd2f7a0416b5f0d322386f667d
Greenwood <magrnw@amazon.com>
2017-03-28 07:32:49 -0400

    addressing @jochenberger's code review comments, Created by... is IntelliJ auto-creation -- added Apache license headers

--- json-path/src/main/java/com/jayway/jsonpath/internal/function/latebinding/PathLateBindingValue.java
+++ json-path/src/main/java/com/jayway/jsonpath/internal/function/latebinding/PathLateBindingValue.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jayway.jsonpath.internal.function.latebinding;
 
 import com.jayway.jsonpath.Configuration;
@@ -10,7 +24,6 @@ import com.jayway.jsonpath.internal.function.ParamType;
  *
  * Acts like a lambda function with references, but since we're supporting JDK 6+, we're left doing this...
  *
- * Created by mattg on 3/27/17.
  */
 public class PathLateBindingValue implements ILateBindingValue {
     private final Path path;
