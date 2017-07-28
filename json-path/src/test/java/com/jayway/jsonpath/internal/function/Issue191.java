a2936ebd6844cf2ae6bf300e24b5469aa788fe1a
Greenwood <magrnw@amazon.com>
2017-07-28 12:06:01 -0400

    inverting the relationship between root scanner and function such that the function takes a parameter which is the scanner expression

--- json-path/src/test/java/com/jayway/jsonpath/internal/function/Issue191.java
+++ json-path/src/test/java/com/jayway/jsonpath/internal/function/Issue191.java
@@ -28,6 +28,14 @@ public class Issue191 {
     }
 
     @Test
+    public void testResultSetNumericComputationTail() {
+        InputStream stream = ClassLoader.getSystemResourceAsStream("issue_191.json");
+        Long value = JsonPath.parse(stream).read("$..timestamp.sum()", Long.class);
+        assertEquals("Expected the max function to consume the aggregation parameters and calculate the max over the result set",
+                Long.valueOf(35679716813L), value);
+    }
+
+    @Test
     public void testMultipleResultSetSums() {
         InputStream stream = ClassLoader.getSystemResourceAsStream("issue_191.json");
         Long value = JsonPath.parse(stream).read("$.sum($..timestamp, $..cpus)", Long.class);
