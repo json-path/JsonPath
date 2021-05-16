831454d10b8a6994b23016bf53e89a6e88c5d868
CindyChow123 <CindyChow123>
2021-05-16 14:51:34 +0800

    modify fix bug 612, add one more testcase

--- json-path/src/test/java/com/jayway/jsonpath/internal/function/Issue612.java
+++ json-path/src/test/java/com/jayway/jsonpath/internal/function/Issue612.java
@@ -25,4 +25,13 @@ public class Issue612 {
         Assert.assertNull(documentContext.read(query));
         Assert.assertNull(documentContext.map(query, (object, configuration) -> object));
     }
+    @Test(expected = Exception.class)
+    public void test2() {
+        Configuration config = Configuration.builder()
+                .build();
+        String json = "{\"1\":{\"2\":null}}";
+        DocumentContext documentContext = JsonPath.using(config).parse(json);
+        JsonPath query = JsonPath.compile("$.1.2.a.b.c");
+        documentContext.map(query, (object, configuration) -> object);
+    }
 }
