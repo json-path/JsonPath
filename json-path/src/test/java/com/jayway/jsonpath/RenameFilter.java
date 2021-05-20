eb79be3ab6f0e53eafaf9e7b9085636f0e45e1b5
TCrazyLion <Tang_CrazyLion@hotmail.com>
2021-05-20 14:02:29 +0800

    Update RenameFilter.java

--- json-path/src/test/java/com/jayway/jsonpath/RenameFilter.java
+++ json-path/src/test/java/com/jayway/jsonpath/RenameFilter.java
@@ -12,9 +12,9 @@ import static org.junit.Assert.assertNotNull;
 
 public class RenameFilter {
 
-    @Test
     //https://github.com/json-path/JsonPath/issues/565
-    public void issue565() {
+    @Test
+    public void issue565_1() {
         String json = "{\n" +
                 "  \"oldName\": {\n" +
                 "    \"oldName\": {\n" +
@@ -38,8 +38,40 @@ public class RenameFilter {
     }
 
     @Test
+    public void issue565_2() {
+        String json = "{\n" +
+                "  \"oldName\": {\n" +
+                "    \"oldName\": {\n" +
+                "      \"oldName\": {\n" +
+                "        \"oldName\": {\n" +
+                "          \"oldName\": {\n" +
+                "            \"oldName\": {\n" +
+                "              \"oldName\": {\n" +
+                "                \"otherName\": \"value\"\n" +
+                "   }\n" +
+                "     }\n" +
+                "       }\n" +
+                "         }\n" +
+                "           }\n" +
+                "             }\n" +
+                "               }\n" +
+                "}";
+        String jsonPath = "$..[?(@.oldName)]";
+        String oldKeyName = "oldName";
+        String newKeyName = "newName";
+        ReadContext context = JsonPath.parse(json).renameKey(jsonPath, oldKeyName, newKeyName);
+        List<Object> result = context.read("$..[?(@.newName)]");
+
+        assertEquals(7, result.size());
+
+        assertNotNull(context.read("$.newName"));
+        assertNotNull(context.read("$.newName.newName"));
+        assertNotNull(context.read("$.newName.newName.newName"));
+    }
+
     //https://github.com/json-path/JsonPath/issues/399
-    public void issue399() {
+    @Test
+    public void issue399_1() {
         String json = "{\n" +
                 "    \"store\": {\n" +
                 "        \"book\": [\n" +
@@ -73,5 +105,41 @@ public class RenameFilter {
         double minPriceViaFilter = ctx.read("$.min($.store.book[?(@.price)].price)");
         double minPriceWildcard = ctx.read("$.min($.store.book[*].price)");
         assertThat(minPriceViaFilter).isEqualTo(minPriceWildcard);
+    }
+
+    @Test
+    public void issue399_2() {
+        String json = "{\n" +
+                "    \"student\": {\n" +
+                "        \"information\": [\n" +
+                "            {\n" +
+                "                \"class\": \"1701\",\n" +
+                "                \"teacher\": \"Wang\",\n" +
+                "                \"id\": \"1701202\",\n" +
+                "                \"age\": 11\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"class\": \"1702\",\n" +
+                "                \"teacher\": \"Zhang\",\n" +
+                "                \"id\": \"1702232\",\n" +
+                "                \"age\": 12\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"class\": \"1703\",\n" +
+                "                \"teacher\": \"Zhu\",\n" +
+                "                \"id\": \"1703656\",\n" +
+                "                \"age\": 13\n" +
+                "            }\n" +
+                "        ]\n" +
+                "   }\n" +
+                "}";
+        ReadContext ctx = JsonPath.parse(json);
+        String output = ctx.read("$.concat($.student.information[?(@.id)].id, \" - \", $.student.information[?(@.teacher)].teacher)");
+        String expected = ctx.read("$.concat($.student.information[*].id, \" - \", $.student.information[*].teacher)");
+        assertThat(output).isEqualTo(expected);
+
+        double minageViaFilter = ctx.read("$.min($.student.information[?(@.age)].age)");
+        double minageWildcard = ctx.read("$.min($.student.information[*].age)");
+        assertThat(minageViaFilter).isEqualTo(minageWildcard);
     }
 }
