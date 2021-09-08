11c31f601e5075973797a538f6a655a1b97f0c1a
Richard Startin <richard@startree.ai>
2021-09-08 10:46:34 +0100

    fix non volatile double checked locking cache initialisation, replace with variant of holder pattern

--- json-path/src/main/java/com/jayway/jsonpath/spi/cache/CacheProvider.java
+++ json-path/src/main/java/com/jayway/jsonpath/spi/cache/CacheProvider.java
@@ -2,31 +2,47 @@ package com.jayway.jsonpath.spi.cache;
 
 import com.jayway.jsonpath.JsonPathException;
 
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+
 import static com.jayway.jsonpath.internal.Utils.notNull;
 
 public class CacheProvider {
-    private static Cache cache;
+
+    private static final AtomicReferenceFieldUpdater<CacheProvider, Cache> UPDATER =
+        AtomicReferenceFieldUpdater.newUpdater(CacheProvider.class, Cache.class, "cache");
+    private static final CacheProvider instance = new CacheProvider();
+
+    private volatile Cache cache;
+
+    private static class CacheHolder {
+        static final Cache CACHE;
+        static {
+            Cache cache = CacheProvider.instance.cache;
+            // the application is trying to use the cache
+            // and if no external implementation has been registered,
+            // we need to initialise it to the default LRUCache
+            if (cache == null) {
+                cache = getDefaultCache();
+                // on the off chance that the cache implementation was registered during
+                // initialisation of the holder, this should be respected, so if the
+                // default cache can't be written back, just read the user supplied value again
+                if (!UPDATER.compareAndSet(instance, null, cache)) {
+                    cache = CacheProvider.instance.cache;
+                }
+            }
+            CACHE = cache;
+        }
+    }
 
     public static void setCache(Cache cache){
         notNull(cache, "Cache may not be null");
-        synchronized (CacheProvider.class){
-            if(CacheProvider.cache != null){
-                throw new JsonPathException("Cache provider must be configured before cache is accessed.");
-            } else {
-                CacheProvider.cache = cache;
-            }
+        if (!UPDATER.compareAndSet(instance, null, cache)) {
+            throw new JsonPathException("Cache provider must be configured before cache is accessed and must not be registered twice.");
         }
     }
 
     public static Cache getCache() {
-        if(CacheProvider.cache == null){
-            synchronized (CacheProvider.class){
-                if(CacheProvider.cache == null){
-                    CacheProvider.cache = getDefaultCache();
-                }
-            }
-        }
-        return CacheProvider.cache;
+        return CacheHolder.CACHE;
     }
 
 
